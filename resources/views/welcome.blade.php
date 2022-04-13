f57ae31ca49c51a690f3f601e5b1fa0436669846
NoPlans4Tonight <jlochan53@gmail.com>
2022-04-13 16:35:45 -0500

    Update: request to add landing page updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    <style>
-        h1 {
-            color: #000000;
-            font-style: normal;
-            font-weight: 500;
-            letter-spacing: 4px;
-            text-transform: uppercase;
-        }
-        p {
-    /* font-family: "Lora", sans-serif; */
-    font-size: 14px;
-    line-height: 25px;
-    font-weight: 300;
-    font-style: italic;
-    text-transform: none;
-    color: #6d6d6d;
-}
-    </style>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -32,22 +15,29 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </head>
-<body id="content">
+<body>
     @include('Navbar')
-      <div id="pic_container" class="container-fluid">
-      <div class="mt-5 mb-2" style="text-align: center">
-          <div>
-            <img src="/images/john-towner-3Kv48NS4WUU-unsplash.jpg" alt="image" height="25%" width="35%" style="border-radius: 2em;">
-          </div>
-      </div>
-    </div>
-    <div class="card-body mt-2" style="text-align: center">
-        <h1 class="px-3">Welcome To Wendie Dalberg's Site</h1>
-        <h2>This Website is still under Development</h2>
-        <p class="px-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet incidunt, temporibus quo, consequuntur at autem veritatis est voluptate repudiandae architecto maiores iure impedit. Repudiandae, qui voluptatem labore veritatis modi optio?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt obcaecati, debitis necessitatibus suscipit doloremque quis, ea voluptatum rerum dignissimos mollitia reiciendis cupiditate delectus, provident reprehenderit quo adipisci voluptas amet sequi.</p>
+    <div class="container card-body mt-5 px-3">
+        &nbsp;
+        <br><br>
+        <h2 style="text-align: center">Wendie T Dalberg LCSW and Associates</h2>
+        <p class="mt-3 mb-3 px-3">We are an inclusive, seasoned, practice with multiple approaches to a common goal. We tailor our work to your individual needs, communication styles and strengths.  You can expect to be listened to keenly, feel understood and be helped to identify the patterns and situations in your life that have led to your seeking assistance. A hallmark of our practice is the belief in the power of the therapeutic relationship in creating an atmosphere which nourishes the ability for change to occur.</p>
+        <h2 class="mt-3 px-3">Who we are:</h2>
+        <br>
+        <p class="mb-3 px-3"><strong>Wendie T Dalberg LCSW</strong>
+        <br>
+        <br>
+        I consider my work to be an extreme privilege.  For over thirty years, I have had the good fortune to work with scores of individuals, couples, and families to help then live more satisfying lives.  My educational background includes a BA form the University of Michigan, and a Masters of Social Work from The Jane Addams School of Social Work at University of Illinois. I’ve availed myself of decades of continuing education workshops.
+        <br>
+        <br>
+        Whether your discomfort comes form outside sources or is more internally driven, skill and true connection in a therapist can help you identify the sources of your struggle(s) and eventually gain a renewed perspective on change. I find that there is often a parallel component of shame, self-rebuking, that comes with discomfort.  It is often, then, a mutual goal between me and my client(s) to help you lift the burden of these feelings and to help create or restore a more positive feeling about yourself.  You might be surprised to find there is often a healthy dose of humor in my work. There is sometimes much relief to be had through laughter, and shared human experiences.
+        <br>
+        <br>
+        I can help you feel less sad, confused, worried, lacking in motivation, unhappy in family, friendship, partnership/couples’ relationships, work dissatisfaction, difficult life circumstances. I can help you feel more hopeful and able to navigate through the situations that had you seek our assistance.  You will be welcomed in our practice and I look forward to meeting you.
+        </p>
     </div>
 </body>
-<footer class="fixed-bottom text-center navbar-light text-lg-start" style="background: #858585;">
+<footer class="absolute-bottom text-center navbar-light text-lg-start" style="background: #858585;">
     <div class="text-center p-3">
       © 2022 Copyright
     </div>
